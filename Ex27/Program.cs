8690b3947a0bd0bf3f7711d32bdd98b9117e66f9
Anastasiya <stasyaya@rambler.ru>
2022-03-28 18:26:19 +0300

    Домашнее задание к 4 семинару

---
+++ Ex27/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int summa = 0;
+
+while (number > 0)
+{
+    summa += number%10;
+    number = number/10; 
+}
+Console.WriteLine("Сумма цифр в числе равна " + summa);
---
+++ Ex29/Program.cs
@@ -0,0 +1,48 @@
+﻿// Напишите программу, которая задаёт массив из 8 случайных целых чисел и выводит отсортированный по модулю массив.
+
+Random rnd = new Random();
+int kol = 8;
+int[] array = new int[kol];
+FillArr();
+PrintArr();
+NewArr();
+PrintArr();
+
+void FillArr()
+{
+    for (int i = 0; i < kol; i++)
+    {
+        array[i] = rnd.Next(-10, 10);
+    }
+}
+void PrintArr()
+{
+    for (int i = 0; i < kol; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine(" ");
+}
+void NewArr()
+{
+    int temp = 0;
+    int index = 0;
+
+    for (int i = 0; i < kol; i++)
+    {
+        index = i;
+        for (int j = i; j < kol; j++)
+        {
+            if (Math.Abs(array[j]) < Math.Abs(array[index]))
+            {
+                index = j;
+            }
+        }
+        if (index != i)
+        {
+            temp = array[i];
+            array[i] = array[index];
+            array[index] = temp;
+        }
+    }
+}
